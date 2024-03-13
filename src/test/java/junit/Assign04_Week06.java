32abced5a416e344c1ddbaacf057ab31c6e79ace
AljaziSultan248 <aljaziskhalaf@gmail.com>
2024-03-13 13:41:55 +0300

    Assignment 4/4

---
+++ src/test/java/junit/Assign04_Week06.java
@@ -0,0 +1,41 @@
+package junit;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
+import java.awt.*;
+
+public class Assign04_Week06 extends testBase{
+//    4/4
+
+
+    @Test
+    public void amazonScrolling() throws AWTException, InterruptedException {
+//        Go to  https://www.amazon.com/stores/page/CDF3296F-98B9-4A3E-BCA2-4834A6E96166
+        bot.navigate("https://www.amazon.com/stores/page/CDF3296F-98B9-4A3E-BCA2-4834A6E96166");
+
+
+//        Scroll to the bottom of the page using a robot
+        Robot robot = new Robot();
+        robot.mouseWheel(4);
+        Thread.sleep(5000);
+
+
+//        Click on the Amazon logo at the bottom of the page (in the footer) using JavascriptExecutor
+            bot.click(By.xpath("//a[contains(@href,'/?ref_=footer_logo')]"));
+
+
+//        Click on the "Back to top" web element
+        driver.findElement(By.xpath("//div[contains(@class,'navFooterBackToTop')]")).click();
+
+
+//       Use Actions to type "ClarusWay" in the search box and perform the search
+        Actions actions = new Actions(driver);
+        By searchBar = By.id("twotabsearchtextbox");
+        actions.moveToElement(driver.findElement(searchBar)).click().sendKeys("ClarusWay").perform();
+
+
+    }
+}
